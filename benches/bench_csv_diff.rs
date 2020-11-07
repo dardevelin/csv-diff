0264dadb4693b152210358feab8dd0920ec5b8c7
Jan Riemer <janriemer@tutanota.de>
2020-11-07 19:14:22 +0100

    refactor(test): refactor bench utils into own crate

--- benches/bench_csv_diff.rs
+++ benches/bench_csv_diff.rs
@@ -8,54 +8,7 @@ use criterion::{
     Criterion
 };
 use csv_diff::csv_diff;
-
-#[derive(Debug)]
-pub struct CsvGenerator {
-    rows: usize,
-    columns: usize,
-}
-
-impl CsvGenerator {
-
-    pub fn new(rows: usize, columns: usize) -> Self {
-        Self {
-            rows,
-            columns,
-        }
-    }
-
-    pub fn generate(&self) -> Vec<u8> {
-        use fake::{
-            Faker,
-            Fake,
-            faker::lorem::en::*
-        };
-        let mut headers = (1..=self.columns).map(|col| format!("header{}", col)).collect::<Vec<_>>().join(",");
-        headers.push('\n');
-        
-        let rows = (0..self.rows())
-            .map(|row_idx| {
-                let mut row: Vec<String> = Words(self.columns..self.columns + 1).fake::<Vec<String>>();
-                row[0] = row_idx.to_string();
-                let mut row_string = row.join(",");
-                row_string.push('\n');
-                row_string.into_bytes()
-            })
-            .flatten()
-            .collect();
-        rows
-    }
-
-    pub fn rows(&self) -> usize {
-        self.rows
-    }
-}
-
-impl Display for CsvGenerator { 
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        write!(f, "{}x{}", self.rows, self.columns)
-    }
-}
+use utils::CsvGenerator;
 
 fn criterion_benchmark(c: &mut Criterion) {
     
@@ -80,11 +33,11 @@ fn criterion_benchmark(c: &mut Criterion) {
         
         bench_group.measurement_time(std::time::Duration::from_secs(
             if csv_gen_left.rows() == 100_000 || csv_gen_right.rows() == 100_000 {
-                15
+                30
             } else if csv_gen_left.rows() == 1_000_000 || csv_gen_right.rows() == 1_000_000 {
-                70
+                100
             } else {
-                5
+                10
             }));
         bench_group.throughput(Throughput::Bytes((csv_left.len() + csv_right.len()) as u64));
         bench_group.bench_with_input(BenchmarkId::from_parameter(format!("{} <> {}", csv_gen_left, csv_gen_right)), &(csv_left, csv_right), |b, (csv_left, csv_right)| {
