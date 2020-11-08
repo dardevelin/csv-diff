594eef64ecadd93ef415bd173c10bb46e4fe7081
Jan Riemer <janriemer@tutanota.de>
2020-11-08 21:44:23 +0100

    test(perf): use 1_000_000 rows for bench_main and activate debug symbols

--- benches/bench_main/src/main.rs
+++ benches/bench_main/src/main.rs
@@ -1,13 +1,13 @@
 use utils::csv_generator::*;
 use csv_diff::csv_diff::*;
+use criterion::black_box;
 
 fn main() {
-    let test = CsvGenerator::new(4,3);
-    dbg!(test);
     let csv_diff = CsvDiff::new();
-    dbg!(csv_diff);
 
-    let (csv_gen_left, csv_gen_right) = (CsvGenerator::new(100_000, 3), CsvGenerator::new(100_000, 3));
+    let (csv_gen_left, csv_gen_right) = (CsvGenerator::new(1_000_000, 3), CsvGenerator::new(1_000_000, 3));
 
-    let res = csv_diff.diff(csv_left.as_slice(), csv_right.as_slice()).unwrap();
+    let (csv_left, csv_right) = (csv_gen_left.generate(), csv_gen_right.generate());
+
+    let res = black_box(csv_diff.diff(csv_left.as_slice(), csv_right.as_slice()).unwrap());
 }
