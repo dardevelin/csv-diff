bdb4304f00098edf9e9f35fbc0784f4a68a245fb
Jan Riemer <janriemer@tutanota.de>
2021-04-05 17:18:32 +0200

    wip: impl thread scope strategy with &self
    This allows us to pass in a rayon thread pool and call `.scope(...)`
    from that thread pool. Nice!

--- src/thread_scope_strategy.rs
+++ src/thread_scope_strategy.rs
@@ -1,13 +1,13 @@
 pub trait ThreadScoper<S> {
-    fn scope<F>(f: F)
+    fn scope<F>(&self, f: F)
     where
         for<'a> F: FnOnce(&'a S) + Send;
 }
 
-pub struct CrossbeamScope {}
+pub struct CrossbeamScope;
 
 impl<'scope> ThreadScoper<crossbeam_utils::thread::Scope<'scope>> for CrossbeamScope {
-    fn scope<F>(f: F)
+    fn scope<F>(&self, f: F)
     where
         for<'a> F: FnOnce(&'a crossbeam_utils::thread::Scope<'scope>),
     {
@@ -15,28 +15,46 @@ impl<'scope> ThreadScoper<crossbeam_utils::thread::Scope<'scope>> for CrossbeamS
     }
 }
 
-struct RayonScope {}
-
-impl<'scope> ThreadScoper<rayon::Scope<'scope>> for RayonScope {
-    fn scope<F>(f: F)
-    where
-        for<'a> F: FnOnce(&'a rayon::Scope<'scope>) + Send,
-    {
-        rayon::scope(|s| f(s));
+impl CrossbeamScope {
+    pub fn new() -> Self {
+        Self
     }
 }
 
+struct RayonScope {
+    thread_pool: rayon::ThreadPool,
+}
+
+impl<'scope> ThreadScoper<rayon::Scope<'scope>> for RayonScope {
+    fn scope<F>(&self, f: F)
+    where
+        for<'a> F: FnOnce(&'a rayon::Scope<'scope>) + Send,
+    {
+        self.thread_pool.scope(|s| f(s));
+    }
+}
+
+impl RayonScope {
+    pub fn new(thread_pool: rayon::ThreadPool) -> Self {
+        Self { thread_pool }
+    }
+}
+
+#[cfg(test)]
 mod tests {
+    use std::sync::atomic::{AtomicU64, Ordering};
+
     use super::{CrossbeamScope, RayonScope, ThreadScoper};
     #[test]
     fn crossbeam_scope_add_num() {
-        let num = std::sync::atomic::AtomicU64::new(0);
-        CrossbeamScope::scope(|s| {
+        let num = AtomicU64::new(0);
+        let crossbeam_scope = CrossbeamScope::new();
+        crossbeam_scope.scope(|s| {
             s.spawn(|_s1| {
-                num.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                num.fetch_add(1, Ordering::SeqCst);
             });
             s.spawn(|_s2| {
-                num.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                num.fetch_add(1, Ordering::SeqCst);
             });
         });
         assert_eq!(2, num.into_inner());
@@ -44,13 +62,14 @@ mod tests {
 
     #[test]
     fn rayon_scope_add_num() {
-        let num = std::sync::atomic::AtomicU64::new(0);
-        RayonScope::scope(|s| {
+        let num = AtomicU64::new(0);
+        let rayon_scope = RayonScope::new(rayon::ThreadPoolBuilder::new().build().unwrap());
+        rayon_scope.scope(|s| {
             s.spawn(|_s1| {
-                num.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                num.fetch_add(1, Ordering::SeqCst);
             });
             s.spawn(|_s2| {
-                num.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                num.fetch_add(1, Ordering::SeqCst);
             });
         });
         assert_eq!(2, num.into_inner());
