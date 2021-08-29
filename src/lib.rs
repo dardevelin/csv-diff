c344b232e133d01939aa9b41812d0d33b0e0a8c3
Jan Riemer <janriemer@tutanota.de>
2021-08-29 18:19:14 +0200

    fix: make module `thread_scope_strategy` private

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,4 @@ pub mod csv_parse_result;
 mod csv_parser_hasher;
 pub mod diff_result;
 pub mod diff_row;
-pub mod thread_scope_strategy; // TODO: do we really need this?
+mod thread_scope_strategy; // TODO: do we really need this?
