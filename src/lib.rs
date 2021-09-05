778b0bc24fbb2c1b9c30b753552b0fc536025bfd
Jan Riemer <janriemer@tutanota.de>
2021-09-05 18:30:17 +0200

    docs: begin documenting the crate as a whole

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+/*!
+Find the difference between two CSVs - with ludicrous speed!
+
+`csv-diff` is a crate for finding the difference between two CSVs.
+It is the fastest CSV-diffing library in the world.
+Comparing two 1,000,000 rows x 9 columns CSVs takes __under 500ms__.
+It is *thread-pool-agnostic*, meaning you can provide your own existing thread-pool
+or you can use the default one that is used in this crate.
+
+# Use Case
+This crate should be used on CSV data that has some sort of *primary key* for uniquely identifying a record.
+It is __not__ a general line-by-line diffing crate.
+You can imagine dumping a database table in CSV format from your *test* and *prod* system and comparing it
+with each other to find differences.
+
+# Overview
+The most important structs and enums you will use are:
+1. [`CsvDiff`](csv_diff/struct.CsvDiff.html) for comparing two CSVs
+2. [`DiffResult`](diff_result/enum.DiffResult.html) for checking whether the two CSVs are different or equal
+3. [`DiffRecords`](diff_result/struct.DiffRecords.html) for the actual differences, if the two CSVs are different.
+*/
+
 pub mod csv_diff;
 mod csv_hash_comparer;
 pub mod csv_hash_task_spawner;
