ce6d49d127127b068a42e8d5e7c764ad49edeb7b
Jan Riemer <janriemer@tutanota.de>
2022-10-09 13:04:43 +0200

    Revert "fix: fix deadlock when diffing >= 10,000 lines"
    This reverts commit 4cd6ab776290f0babdb16d45826b581cceba9783.
    
    NOTE: We revert, because it turns out that statically determining the
    threshold for the max capacity for the hashmaps is _less performant_
    than calculating it dynamically ("on the fly").
    We will implement the dynamically calculated threshold again in the next
    commits.

--- src/csv_parse_result.rs
+++ src/csv_parse_result.rs
@@ -108,12 +108,17 @@ impl CsvByteRecordWithHash {
 #[derive(Debug)]
 pub struct CsvByteRecordWithHashFirstFewLines {
     pub(crate) records: Vec<CsvByteRecordWithHash>,
+    //num_of_lines_hint: u64,
 }
 
 impl CsvByteRecordWithHashFirstFewLines {
-    pub(crate) fn new(records: impl IntoIterator<Item = CsvByteRecordWithHash>) -> Self {
+    pub(crate) fn new(
+        records: impl IntoIterator<Item = CsvByteRecordWithHash>,
+        //num_of_lines_hint: u64,
+    ) -> Self {
         Self {
             records: records.into_iter().collect(),
+            //num_of_lines_hint,
         }
     }
 }
